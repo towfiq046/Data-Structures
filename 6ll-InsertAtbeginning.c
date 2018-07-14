5e4bcbe88e28d3ad5de5eac81c22322f89e38ca9
towfiq046 <towfiqahmed046@gmail.com>
2018-07-14 11:31:38 +0600

    head is null-- descriptive part

---
+++ 6ll-InsertAtbeginning.c
@@ -0,0 +1,47 @@
+// linked-list: insert a node at the beginning.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+    int data;
+    struct node* next;
+};
+
+struct node* head;   // global variable. // head is an indication of the first node.
+
+void insert(int inputFromUser) {
+
+    // creating a node.
+    node* temp = (node*)malloc(sizeof(struct node));    // variable temp, pointer to node. // storing the address of node in temp.
+    // dereferencing (*temp) for accessing the fields (in this case data part) of the node.
+
+    (*temp).data = inputFromUser;   // or-- temp->data = inputFromUser;
+    // dereferencing the pointer variable (temp) to modify the node.
+    // inputFromUser is in data part with the help of (dereferencing) *temp.
+
+    temp->next = NULL;    //  nothing in next.
+
+    if(head != NULL)
+        temp->next = head;      // head is now next element. then--
+
+    head = temp;    // head has the address of the 'first' node.
+}
+
+int main() {
+
+    head = NULL;        // empty list.
+    printf("How many elements? ");
+
+    int n, i, inputFromUser;
+    scanf("%d", &n);
+
+    for(i = 0; i < n; i++) {
+        printf("Enter number: ");
+        scanf("%d", &inputFromUser);
+
+        insert(inputFromUser);
+        print();
+    }
+}
+
